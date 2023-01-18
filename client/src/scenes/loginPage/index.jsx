5a48dfef3391240da492bd2f698b2925019e38ed
renandantasp <renandantasp@gmail.com>
2023-01-18 06:53:19 -0300

    feat(login): add login/register page

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,5 +1,38 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material"
+import Form from "./Form"
+
 const LoginPage = () => {
-    return (<div>LoginPage</div>)
+    const theme = useTheme();
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px")
+
+    return <Box>
+            <Box 
+                width="100%" 
+                backgroundColor={theme.palette.background.alt}
+                p="1rem 6%"
+                textAlign="center"
+                >
+                    <Typography 
+                        fontWeight="bold" 
+                        fontSize="32px"
+                        color="primary"
+                        > DailyHub 
+                    </Typography>
+                </Box>
+                <Box 
+                width={isNonMobileScreens ? "50%" : "93%"}
+                p="2rem"
+                m="2rem auto"
+                borderRadius="1.5rem"
+                backgroundColor={theme.palette.background.alt}
+                >
+                    <Typography fontWeight="500" variant="h5" sx={{mb: "1.5 rem"}}>
+                        Welcome to DailyHub, you daily hub for daily stuff!
+                    </Typography>
+                    <Form/>
+            </Box>
+    </Box>
+
 }
 
 export default LoginPage
--- server/index.js
+++ server/index.js
@@ -14,11 +14,6 @@ import userRoutes from './routes/users.js'
 import { register } from "./controllers/auth.js"
 import { createPost } from "./controllers/posts.js"
 import { verifyToken } from "./middleware/auth.js"
-import Post from './models/Post.js'
-import User from './models/User.js'
-import { users, posts } from "./data/index.js"
-
-/* CONF */
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -64,7 +59,4 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
 }).then(() => {
     app.listen(PORT, () => console.log(`Server port: ${PORT}`))
-    
-    //User.insertMany(users)
-    //Post.insertMany(posts)
 }).catch((error) => console.log(`${error} did not connect`))
