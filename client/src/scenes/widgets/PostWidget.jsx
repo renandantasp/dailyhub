b11a5f9a830cbd7f7e62a8ab95ff62e0b5f6f23e
renandantasp <renandantasp@gmail.com>
2023-02-01 16:20:20 -0300

    feat(client): add PostWidget setup

---
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -0,0 +1,47 @@
+import { 
+    ChatBubbleOutlineOutlined, 
+    FavoriteBorderOutlined, 
+    FavoriteOutlined, 
+    ShareOutlined 
+} from "@mui/icons-material";
+
+import { 
+    Box, 
+    Divider, 
+    IconButton, 
+    Typography, 
+    useTheme 
+} from '@mui/material'
+
+import FlexBetween from "components/FlexBetween";
+import Friend from "components/Friend"
+import WidgetWrapper from "components/WidgetWrapper";
+import { useState } from "react"
+import { useDispatch, useSelector } from "react-redux";
+import { setPost } from "state";
+
+const PostWidget = ({
+    key,
+    postId,
+    postUserId,
+    name,
+    description,
+    location,
+    picturePath,
+    userPicturePath,
+    likes,
+    comments,
+}) => {
+    const [isComments, setIsComments] = useState(false)
+    const dispatch = useDispatch()
+    const token = useSelector((state)=> state.token)
+    const loggedInUserId = useSelector((state) => state.user._id)
+    const isLiked = Boolean(likes[loggedInUserId])
+    const likeCont = Object.keys(likes).length
+    
+    const { palette } = useTheme()
+    const main = palette.neutral.main
+    const primary = palette.primary.main
+}
+
+export default PostWidget
